7c3ee22788697678b4460622e23f09ae184fbb66
Phil Shapiro <pshapiro@broadinstitute.org>
2021-07-15 13:54:17 -0400

    Add "wdl" support; this is just a start and many things don't work or aren't done correctly yet.

--- src/components/WDLViewer.js
+++ src/components/WDLViewer.js
@@ -64,10 +64,112 @@ Prism.languages.wdl = {
   ]
 }
 
+const wdlMonarch = {
+  keywords: [
+    'version', 'call', 'runtime', 'task', 'workflow', 'if', 'then', 'else',
+    'import', 'as', 'input', 'output', 'meta', 'parameter_meta', 'scatter', 'struct',
+    'object', 'command'
+  ],
+
+  typeKeywords: [
+    'Array', 'Boolean', 'File', 'Float', 'Int', 'Map', 'Object', 'String', 'Pair'
+  ],
+
+  operators: [
+    '=', '!', '*', '<', '>', '+', '-', '/', '%', '&&'
+  ],
+
+  symbols: /[=><!~?:&|+\-*\/\^%]+/,
+
+  // C# style strings
+  escapes: /\\(?:[abfnrtv\\"']|x[0-9A-Fa-f]{1,4}|u[0-9A-Fa-f]{4}|U[0-9A-Fa-f]{8})/,
+
+  // The main tokenizer for our languages
+  tokenizer: {
+    root: [
+      // identifiers and keywords
+      [/[a-z_$][\w$]*/, {
+        cases: {
+          '@typeKeywords': 'keyword',
+          '@keywords': 'keyword',
+          '@default': 'identifier'
+        }
+      }],
+      [/[A-Z][\w\$]*/, 'type.identifier'], // to show class names nicely
+
+      // whitespace
+      { include: '@whitespace' },
+
+      // delimiters and operators
+      [/[{}()\[\]]/, '@brackets'],
+      [/[<>](?!@symbols)/, '@brackets'],
+      [/@symbols/, {
+        cases: {
+          '@operators': 'operator',
+          '@default': ''
+        }
+      }],
+
+      // @ annotations.
+      // As an example, we emit a debugging log message on these tokens.
+      // Note: message are supressed during the first load -- change some lines to see them.
+      [/@\s*[a-zA-Z_\$][\w\$]*/, { token: 'annotation', log: 'annotation token: $0' }],
+
+      // numbers
+      [/\d*\.\d+([eE][\-+]?\d+)?/, 'number.float'],
+      [/0[xX][0-9a-fA-F]+/, 'number.hex'],
+      [/\d+/, 'number'],
+
+      // delimiter: after number because of .\d floats
+      [/[;,.]/, 'delimiter'],
+
+      // strings
+      [/"([^"\\]|\\.)*$/, 'string.invalid'], // non-teminated string
+      [/"/, { token: 'string.quote', bracket: '@open', next: '@string' }],
+
+      // characters
+      [/'[^\\']'/, 'string'],
+      [/(')(@escapes)(')/, ['string', 'string.escape', 'string']],
+      [/'/, 'string.invalid']
+    ],
+
+    comment: [
+      [/#.*/, 'comment']
+    ],
+
+    string: [
+      [/[^\\"]+/, 'string'],
+      [/@escapes/, 'string.escape'],
+      [/\\./, 'string.escape.invalid'],
+      [/"/, { token: 'string.quote', bracket: '@close', next: '@pop' }]
+    ],
+
+    whitespace: [
+      [/[ \t\r\n]+/, 'white'],
+      [/\/\*/, 'comment', '@comment'],
+      [/\/\/.*$/, 'comment']
+    ]
+  }
+}
+
+
+const handleEditorWillMount = monaco => {
+  // here is the monaco instance
+  // do something before editor is mounted
+  monaco.languages.register({
+    id: 'wdl'
+  })
+  monaco.languages.setMonarchTokensProvider('wdl', wdlMonarch)
+}
+
+
 const WDLViewer = ({ wdl }) => {
-  return wdl && div({ style: { width: 500 } }, [h(Editor, {
+  return div({ style: { width: 500 } }, [h(Editor, {
     height: '90vh',
-    value: wdl
+    width: 1500,
+    language: 'wdl',
+    value: wdl,
+    beforeMount: handleEditorWillMount
   })])
 }
 
