255ae3052490db6994beedb1f5b7b242d074fce6
Phil Shapiro <pshapiro@broadinstitute.org>
2021-07-15 13:55:28 -0400

    eslint fixes

--- src/components/WDLViewer.js
+++ src/components/WDLViewer.js
@@ -79,7 +79,7 @@ const wdlMonarch = {
     '=', '!', '*', '<', '>', '+', '-', '/', '%', '&&'
   ],
 
-  symbols: /[=><!~?:&|+\-*\/\^%]+/,
+  symbols: /[=><!~?:&|+\-*/^%]+/,
 
   // C# style strings
   escapes: /\\(?:[abfnrtv\\"']|x[0-9A-Fa-f]{1,4}|u[0-9A-Fa-f]{4}|U[0-9A-Fa-f]{8})/,
@@ -95,13 +95,13 @@ const wdlMonarch = {
           '@default': 'identifier'
         }
       }],
-      [/[A-Z][\w\$]*/, 'type.identifier'], // to show class names nicely
+      [/[A-Z][\w$]*/, 'type.identifier'], // to show class names nicely
 
       // whitespace
       { include: '@whitespace' },
 
       // delimiters and operators
-      [/[{}()\[\]]/, '@brackets'],
+      [/[{}()[\]]/, '@brackets'],
       [/[<>](?!@symbols)/, '@brackets'],
       [/@symbols/, {
         cases: {
@@ -113,10 +113,10 @@ const wdlMonarch = {
       // @ annotations.
       // As an example, we emit a debugging log message on these tokens.
       // Note: message are supressed during the first load -- change some lines to see them.
-      [/@\s*[a-zA-Z_\$][\w\$]*/, { token: 'annotation', log: 'annotation token: $0' }],
+      [/@\s*[a-zA-Z_$][\w$]*/, { token: 'annotation', log: 'annotation token: $0' }],
 
       // numbers
-      [/\d*\.\d+([eE][\-+]?\d+)?/, 'number.float'],
+      [/\d*\.\d+([eE][-+]?\d+)?/, 'number.float'],
       [/0[xX][0-9a-fA-F]+/, 'number.hex'],
       [/\d+/, 'number'],
 
