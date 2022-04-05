2dfbff1489e1fb4ee0063958e38bb71e38970e4f
Saloni Shah <salonipshah11@gmail.com>
2022-04-05 17:29:42 -0400

    Hello World UI; No deletions yet

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,43 +1,11 @@
 import 'src/libs/routes'
 
 import { h } from 'react-hyperscript-helpers'
-import ReactNotification from 'react-notifications-component'
-import AuthContainer from 'src/components/AuthContainer'
-import ConfigOverridesWarning from 'src/components/ConfigOverridesWarning'
-import CookieWarning from 'src/components/CookieWarning'
-import ErrorWrapper from 'src/components/ErrorWrapper'
-import Favicon from 'src/components/Favicon'
-import FirecloudNotification from 'src/components/FirecloudNotification'
-import IdleStatusMonitor from 'src/components/IdleStatusMonitor'
-import ImportStatus from 'src/components/ImportStatus'
-import NpsSurvey from 'src/components/NpsSurvey'
-import { AuthenticatedCookieSetter } from 'src/components/runtime-common'
-import ServiceAlerts from 'src/components/ServiceAlerts'
-import SupportRequest from 'src/components/SupportRequest'
-import { PageViewReporter } from 'src/libs/events'
-import { LocationProvider, PathHashInserter, Router, TitleManager } from 'src/libs/nav'
+import { HelloWorld } from 'src/components/HelloWorld'
 
 
 const Main = () => {
-  return h(LocationProvider, [
-    h(PathHashInserter),
-    h(CookieWarning),
-    h(ReactNotification),
-    h(ImportStatus),
-    h(ServiceAlerts),
-    h(Favicon),
-    h(IdleStatusMonitor),
-    h(ErrorWrapper, [
-      h(TitleManager),
-      h(FirecloudNotification),
-      h(AuthenticatedCookieSetter),
-      h(AuthContainer, [h(Router)])
-    ]),
-    h(PageViewReporter),
-    h(SupportRequest),
-    h(NpsSurvey),
-    h(ConfigOverridesWarning)
-  ])
+  return h(HelloWorld)
 }
 
 export default Main
